8071377b22a4553bd0da872a0541396e36f7cd93
supersuhyeon <suhyeonkr@gmail.com>
2022-08-14 23:53:10 -0700

    refactoring using npm, sass

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-'use strict'
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle";
 
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
-
-
-const emailInput = document.querySelector('#exampleInputEmail1')
-const modalOpen = document.querySelector('#exampleModal')
-
-modalOpen.addEventListener('shown.bs.modal', ()=>{
-    emailInput.focus()
-})
