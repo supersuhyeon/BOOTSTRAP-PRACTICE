37bc6cdc0c8700729dccc1da1a321c0aadc4b0c3
supersuhyeon <suhyeonkr@gmail.com>
2022-08-15 00:35:40 -0700

    optimizing javascript

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle";
+import Dropdown from "bootstrap/js/dist/dropdown";
+import Modal from "bootstrap/js/dist/modal";
 
+const dropdownElementList = Array.prototype.slice.call(document.querySelectorAll('.dropdown-toggle'))
+dropdownElementList.map(function (dropdownToggleEl) {return new Dropdown(dropdownToggleEl)})
+
+new Modal(document.querySelector('#exampleModal'), {
+    backdrop:'static'
+})
+
+// backdrop: Includes a modal-backdrop element. Alternatively, specify static for a backdrop which doesn’t close the modal when clicked.
